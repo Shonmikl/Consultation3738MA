3df84b58001d2f68840b8f7a748736afc069ed85
shonmikl <mikhailegorov676@gmail.com>
2022-09-30 13:47:43 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String model;
+    double horsePower;
+
+    public void drive() {
+        //save id;
+    }
+
+    public int getResult(int a, int b) {
+        int c = a * b;
+        return 'c';
+    } // a * b; число
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+     *  Напишите метод, который принимает в качестве параметра int и
+     *  при помощи scanner, int result = 3.14*a*a sout(result):
+     * “The area of a circle with radius …..”.
+     * */
+
+    public static void getCircleSquare(float a) {
+        System.out.println
+                ("The area of a circle with radius: [" + a + "] is " + (3.14 * a * a));
+    }
+
+   public static float getSum(int a, int b) {
+        return a + b;
+   }
+
+    public static void main(String[] args) {
+        float a = getSum(1, 2);
+
+
+        Scanner scanner = new Scanner(System.in);
+        ;//ждет ввода с клавиатуры
+       // System.out.println("Please input a radius... ");
+        // getCircleSquare(scanner.nextInt());
+        //*******************************************************//
+//        int a = scanner.nextInt();
+//        System.out.println
+//                ("The area of a circle with radius: [" + a + "] is " + (3.14 * a * a));
+
+        double a = 8;
+        double b = 3;
+        double c = a / b;
+        System.out.println(c);
+    }
+}
